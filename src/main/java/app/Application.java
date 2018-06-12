36867e676f10825092439566ce812cfde90a3ee3
Vasil Filipov <filipov.v@icloud.com>
2018-06-12 17:34:46 +0300

    Added functionality

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,7 +1,27 @@
 package app;
 
+import app.models.Car;
+
 public class Application {
     public static void main(String[] args) {
+        Car car = new Car(4, 5, "Black", 2014, "Left-Handed", "Manual", 4, 1600, 4.5);
 
+        car.printAllInfo();
+
+        String checkColor = "black";
+        System.out.printf("Is car colored %s: %s%n", checkColor, car.isColored(checkColor));
+
+        int checkShift = 5;
+        System.out.printf("Is current shift %d: %s%n", checkShift, car.isShiftAs(checkShift));
+
+        car.changeCurrentShiftUp();
+        car.changeCurrentShiftDown();
+
+        int addPassengers = 4;
+        System.out.printf("Is there space for %d passengers in the car: %s%n", addPassengers, car.isSpaceEnoughForNumberOfPassengers(addPassengers));
+        System.out.println("Weight is under 3500: "+car.isWeightUnder3500kg(4, 4));
+
+        int distance = 75;
+        System.out.printf("Fuel needed for %dkm - %.2f%n", distance, car.fuelNeededToReachDestination(distance));
     }
 }
--- src/main/java/app/models/Car.java
+++ src/main/java/app/models/Car.java
@@ -23,11 +23,55 @@ public class Car {
         this.setConsumption(consumption);
     }
 
+    public void printAllInfo() {
+        System.out.printf("Car info:%n" +
+                "Doors: %d%n" +
+                "Seats: %d%n" +
+                "Color: %s%n" +
+                "Year Created: %d%n" +
+                "Steering Wheel: %s%n" +
+                "Gear Shifts: %s%n" +
+                "Current Shift: %d%n" +
+                "Weight: %d%n" +
+                "Consumption: %.2f%n",
+                this.getDoors(), this.getSeats(), this.getColor(), this.getYear(), this.getSteeringWheel(), this.getGearShift(), this.getCurrentShift(), this.getWeight(), this.getConsumption());
+    }
+
+    public boolean isColored(String color) {
+        return color.equalsIgnoreCase(this.getColor());
+    }
+
+    public boolean isShiftAs(int shift) {
+        return shift==this.getCurrentShift();
+    }
+
+    public void changeCurrentShiftUp() {
+        this.currentShift++;
+        System.out.println("Current shift changed to "+this.getCurrentShift());
+    }
+
+    public void changeCurrentShiftDown() {
+        System.out.println("Current shift changed to "+ --this.currentShift);
+    }
+
+    public boolean isSpaceEnoughForNumberOfPassengers(int numberOfPassengers) {
+        return this.getSeats()-1>=numberOfPassengers;
+    }
+
+    public boolean isWeightUnder3500kg(int numberOfBags, int numberOfPassengers) {
+        int totalAddedWeight = numberOfBags*40+numberOfPassengers*60;
+        return this.getWeight()+totalAddedWeight<3500;
+    }
+
+    public double fuelNeededToReachDestination(int destinationDistance) {
+        return this.getConsumption()/100*destinationDistance;
+    }
+
     public int getDoors() {
         return doors;
     }
 
-    public void setDoors(int doors) {
+    private void setDoors(int doors) {
         this.doors = doors;
     }
 
@@ -35,7 +79,7 @@ public class Car {
         return seats;
     }
 
-    public void setSeats(int seats) {
+    private void setSeats(int seats) {
         this.seats = seats;
     }
 
@@ -43,7 +87,7 @@ public class Car {
         return color;
     }
 
-    public void setColor(String color) {
+    private void setColor(String color) {
         this.color = color;
     }
 
@@ -51,7 +95,7 @@ public class Car {
         return year;
     }
 
-    public void setYear(int year) {
+    private void setYear(int year) {
         this.year = year;
     }
 
@@ -59,7 +103,7 @@ public class Car {
         return steeringWheel;
     }
 
-    public void setSteeringWheel(String steeringWheel) {
+    private void setSteeringWheel(String steeringWheel) {
         this.steeringWheel = steeringWheel;
     }
 
@@ -67,7 +111,7 @@ public class Car {
         return gearShift;
     }
 
-    public void setGearShift(String gearShift) {
+    private void setGearShift(String gearShift) {
         this.gearShift = gearShift;
     }
 
@@ -75,7 +119,7 @@ public class Car {
         return currentShift;
     }
 
-    public void setCurrentShift(int currentShift) {
+    private void setCurrentShift(int currentShift) {
         this.currentShift = currentShift;
     }
 
@@ -83,7 +127,7 @@ public class Car {
         return weight;
     }
 
-    public void setWeight(int weight) {
+    private void setWeight(int weight) {
         this.weight = weight;
     }
 
@@ -91,7 +135,7 @@ public class Car {
         return consumption;
     }
 
-    public void setConsumption(double consumption) {
+    private void setConsumption(double consumption) {
         this.consumption = consumption;
     }
 }
